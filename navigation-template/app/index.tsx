533d362359f5c06fbac732574564b8c26e7600cc
PlatenPrime <proshta@gmail.com>
2023-08-03 09:05:54 +0300

    03.08.2023

--- navigation-template/app/index.tsx
+++ navigation-template/app/index.tsx
@@ -2,6 +2,8 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { Appearance, useColorScheme } from 'react-native';
+import { Link } from 'expo-router';
+
 
 
 const index = () => {
@@ -9,12 +11,20 @@ const index = () => {
 	let colorScheme = useColorScheme();
 
 
+
 	return (
 		<SafeAreaProvider>
 			<View>
 				<Text>My App
 
-					
+					<View>
+						<Link href='/animation' >Animation</Link>
+					</View>
+
+
+					<View>
+						<Link href='/uisand' >UI Sandbox</Link>
+					</View>
 
 				</Text>
 			</View>
